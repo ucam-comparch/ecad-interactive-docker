f5e73a8cdb312b41e98f9237ae4c6fe3c070f56f
JonasAlaif <jonas.fiala.cardiff@gmail.com>
2020-09-01 13:14:37 +0100

    Add comments

--- bin/execute-spike.sh
+++ bin/execute-spike.sh
@@ -7,11 +7,11 @@ cd $SUBMISSIONDIR/test_env
 
 LOG=$(spike --isa=rv32i $ARGS -m0x80000000:0x10000 build/*.elf 2>&1 || true)
 echo "$LOG"
-retVal=`echo "$LOG" | grep -oP "(?<=tohost = )[0-9]*"`
+retVal=`echo "$LOG" | grep -oP "(?<=tohost = )[0-9]*"`        # Spike outputs instruction trace and then "*** FAILED *** (tohost = N)" where N is the error code
 if [ $retVal -ne 0 ]; then
-  if [ $((retVal % 2)) -eq 0 ]; then
-    grep -oP "(?<=TEST$((retVal / 2))DIV: ).*" ./src/main.c
-  else
+  if [ $((retVal % 2)) -eq 0 ]; then                          # If even, failed on division, otherwise on modulo
+    grep -oP "(?<=TEST$((retVal / 2))DIV: ).*" ./src/main.c   # Grep in the testing source code for the correct answers
+  else                                                        # Which are put there when the file is generated
     grep -oP "(?<=TEST$((retVal / 2))MOD: ).*" ./src/main.c
   fi
   exit 1
