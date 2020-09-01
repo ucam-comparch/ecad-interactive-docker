bf0ed8ad39d1a5bdb974f78fa247ea128d2cbd8d
JonasAlaif <jonas.fiala.cardiff@gmail.com>
2020-09-01 13:18:03 +0100

    More comments

--- bin/execute-spike.sh
+++ bin/execute-spike.sh
@@ -6,7 +6,7 @@ ARGS=$2
 cd $SUBMISSIONDIR/test_env
 
 LOG=$(spike --isa=rv32i $ARGS -m0x80000000:0x10000 build/*.elf 2>&1 || true)
-echo "$LOG"
+echo "$LOG"                                                   # All of spike output is to stderr for some reason so redirect it to stdout
 retVal=`echo "$LOG" | grep -oP "(?<=tohost = )[0-9]*"`        # Spike outputs instruction trace and then "*** FAILED *** (tohost = N)" where N is the error code
 if [ $retVal -ne 0 ]; then
   if [ $((retVal % 2)) -eq 0 ]; then                          # If even, failed on division, otherwise on modulo
