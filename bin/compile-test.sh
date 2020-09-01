1a1218f637415104d83bca876664a3d3966aacea
JonasAlaif <jonas.fiala.cardiff@gmail.com>
2020-09-01 13:06:21 +0100

    Update scripts to current

--- bin/compile-test.sh
+++ bin/compile-test.sh
@@ -4,4 +4,5 @@ TASKDIR=$1
 
 mkdir -p $TASKDIR/common
 cp -R $TASKDIR/test/assembly/. $TASKDIR/common/
+chown -R user:user $TASKDIR/common/
 
--- bin/execute-spike.sh
+++ bin/execute-spike.sh
@@ -4,5 +4,17 @@ SUBMISSIONDIR=$1
 ARGS=$2
 
 cd $SUBMISSIONDIR/test_env
-spike --isa=rv32i $ARGS -m0x80000000:0x10000 build/*.elf
+
+LOG=$(spike --isa=rv32i $ARGS -m0x80000000:0x10000 build/*.elf 2>&1 || true)
+echo "$LOG"
+retVal=`echo "$LOG" | grep -oP "(?<=tohost = )[0-9]*"`
+if [ $retVal -ne 0 ]; then
+  if [ $((retVal % 2)) -eq 0 ]; then
+    grep -oP "(?<=TEST$((retVal / 2))DIV: ).*" ./src/main.c
+  else
+    grep -oP "(?<=TEST$((retVal / 2))MOD: ).*" ./src/main.c
+  fi
+  exit 1
+fi
+exit 0
 
